c8d93af25c4e08e69c4523e86666aa56533d3d24
David Redding <redding.david35@gmail.com>
2022-05-23 09:19:40 -0500

    Routing

--- src/App.js
+++ src/App.js
@@ -1,3 +1,24 @@
-const App = () => <div>Hi</div>;
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Signup from './pages/signup/Signup';
+import Login from './pages/login/Login';
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
